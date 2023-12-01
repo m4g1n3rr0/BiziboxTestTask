d9f8c44d5378e0ac1703cff46ae8c9c1278eefb7
petrlarionov <petrlarionov@192.168.1.132>
2023-12-01 23:48:06 +0200

    Checks about server working and information saving are added

--- DataReceiving/src/main/java/com/example/DataReceiving/controller/Controller.java
+++ DataReceiving/src/main/java/com/example/DataReceiving/controller/Controller.java
@@ -1,7 +1,48 @@
 package com.example.DataReceiving.controller;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.DataReceiving.model.PersonsNumbers;
+import com.example.DataReceiving.repo.CallRepo;
+
+@RestController
+@RequestMapping("/PhineBook")
 public class Controller {
 	
+	@Autowired
+	private CallRepo callRepo;
 	
+	@PostMapping
+	public ResponseEntity registration(@RequestBody PersonsNumbers phoneNumber) {
+	
+		try {
+			
+			callRepo.save(phoneNumber);
+			return ResponseEntity.ok("Information aboutИ call was successfully saved");
+			
+		} catch (Exception e) {
+			
+			return ResponseEntity.badRequest().body("Error was happened");
+			
+		}
+		
+	}
+	
+	@GetMapping
+	public ResponseEntity getUsers() {
+	
+		try {
+			
+			return ResponseEntity.ok("Server is working");
+			
+		} catch (Exception e) {
+			
+			return ResponseEntity.badRequest().body("Error was happened");
+			
+		}
+		
+	}
 	
 }
