761ac7b49ceab5b9d3eadc25a22f170a32c6e69d
petrlarionov <petrlarionov@192.168.1.132>
2023-12-01 17:42:46 +0200

    Updated fields for DB

--- DataReceiving/src/main/java/com/example/DataReceiving/model/PersonsNumbers.java
+++ DataReceiving/src/main/java/com/example/DataReceiving/model/PersonsNumbers.java
@@ -1,7 +1,20 @@
 package com.example.DataReceiving.model;
 
+import lombok.Data;
+import org.springframework.*;
+
+import jakarta.persistence.*;
+
+
+@Data
+@Entity
+@Table(name = "PhoneBook")
 public class PersonsNumbers {
 	
-	
-	
+	@Id
+	private long phoneNumber;
+	private String time;
+	private String incOut;
+	private String duration;
+
 }
