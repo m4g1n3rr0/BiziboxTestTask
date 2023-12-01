5df8595779757fc3f66f31fffe687b3a12ac7bac
petrlarionov <petrlarionov@192.168.1.132>
2023-12-01 15:59:45 +0200

    Building the project

---
+++ DataReceiving/src/main/java/com/example/DataReceiving/DataReceivingApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataReceiving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataReceivingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataReceivingApplication.class, args);
+	}
+
+}
---
+++ DataReceiving/src/test/java/com/example/DataReceiving/DataReceivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataReceiving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataReceivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
